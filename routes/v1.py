e359a3f3ae2bf10ab4b7f979c92ad531cf11580b
mateusleao <mateusleao81@gmail.com>
2021-04-26 18:28:49 -0500

    adding middleware to our api, and the ppt from the course

--- routes/v1.py
+++ routes/v1.py
@@ -5,7 +5,7 @@ from models.book import Book
 from starlette.status import HTTP_201_CREATED, HTTP_401_UNAUTHORIZED
 from starlette.responses import Response
 from fastapi.security import OAuth2PasswordRequestForm
-from utils.security import authenticate_user, create_jwt_token, check_jwt_token
+from utils.security import authenticate_user, create_jwt_token
 from models.jwt_user import JWTUser
 # For the client to be able to consume our api he has to pass a "authorization"
 # "attribute" in the Header of the request containing "Bearer asuijaruoqw8928912389123iu" (Bearer + token)
@@ -16,10 +16,29 @@ app_v1 = FastAPI(openapi_prefix="/v1")
 async def hello_world():
     return {"request body": "hello world from FastAPI"}
 
+
+@app_v1.post("/token")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    """
+    User sends it's username and password, if this validation is passed
+    then our app generates a web token for us.
+    :param form_data:
+    :return: Returns the JSON Web Token if the username and password were correctly validated.
+    """
+    jwt_user_dict = {"username" : form_data.username,
+                     "password" : form_data.password}
+    jwt_user = JWTUser(**jwt_user_dict)
+    user = authenticate_user(jwt_user)
+    if user is None: # we are setting this in authentica_user if auth fails
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED)
+    jwt_token = create_jwt_token(user)
+    return {"token" : jwt_token}
+
+
 # add custom headers in the user endpoint, defined inside function post_user
 @app_v1.post("/user", status_code=HTTP_201_CREATED)
 # when sending the request we use x-custom, _ becomes -
-async def post_user(user: User, x_custom: str = Header(...), jwt: bool = Depends(check_jwt_token)):
+async def post_user(user: User, x_custom: str = Header(...)):
     return {"request body": user, "request custom header" : x_custom}
 
 
@@ -47,7 +66,7 @@ async def get_book_with_isbn(name: str):
 
 
 @app_v1.get("/author/{id}/book")
-async def get_authors_books(id: int, category: str, order: str = "asc", ):  # query parameters
+async def get_authors_books(id: int, category: str, order: str = "asc"):  # query parameters
     return {"query parameter": order + category + str(id)}
 
 
@@ -70,13 +89,4 @@ async def upload_user_photo(response: Response, profile_photo: bytes = File(...)
     response.set_cookie(key="cookie-api", value="test")
     return {"file size" : len(profile_photo)}
 
-@app_v1.post("/token")
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    jwt_user_dict = {"username" : form_data.username,
-                     "password" : form_data.password}
-    jwt_user = JWTUser(**jwt_user_dict)
-    user = authenticate_user(jwt_user)
-    if user is None: # we are setting this in authentica_user if auth fails
-        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED)
-    jwt_token = create_jwt_token(user)
-    return {"token" : jwt_token}
+
